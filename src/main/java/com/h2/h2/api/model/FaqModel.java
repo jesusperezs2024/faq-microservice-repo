cf8a1584df0571a32590f70c258515cc5a15ba5b
fesh2025 <correo@ejemplo.com>
2025-01-28 21:38:50 -0500

    ajutes faq para relacion con Azure

--- src/main/java/com/h2/h2/api/model/FaqModel.java
+++ src/main/java/com/h2/h2/api/model/FaqModel.java
@@ -4,28 +4,16 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name = "faqTable")
+@Table(name = "faqtable")
 @Data
 public class FaqModel {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long idFaq;
-    
-        @Column(name = "question",length = 120, nullable = false)
+        private Integer idfaq;
         private String question;
-    
-        @Column(name = "answer",length = 120, nullable = false)
         private String answer;
-    
-        @Column(name = "category",length = 120, nullable = false)
         private String category;
-    
-        @Column(name = "status",length = 120, nullable = false)
         private String status;
-    
-        @Column(name = "createUser",length = 120, nullable = false)
         private String createUser;
-    
-        @Column(name = "updateUser",length = 120, nullable = true)
         private String updateUser;
 }
